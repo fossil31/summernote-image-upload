b7970437cf4f70e2f4814fae26cba27f7a6baac5
Samrith Soikaew <bios_rith@hotmail.com>
2020-10-08 09:20:32 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -11,3 +11,19 @@ CREATE TABLE `test` (
   KEY `id` (`id`),
   KEY `ref_id` (`ref_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `summernote_upload`;
+CREATE TABLE `summernote_upload` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(50) NOT NULL DEFAULT '',
+  `file_type` varchar(50) DEFAULT NULL,
+  `extension` varchar(50) DEFAULT NULL,
+  `file_size` decimal(10,2) DEFAULT NULL,
+  `file_path` varchar(250) NOT NULL DEFAULT '',
+  `original_name` varchar(200) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `created_by` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `file_name` (`file_name`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='upload image from summernote to database';
